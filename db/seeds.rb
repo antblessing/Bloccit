ddc043233df136440bc462f1fa19d5b3d63c6b92
antblessing <antblessing@gmail.com>
2015-09-30 18:28:36 +0000

    checkpoint_44

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,11 +36,18 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'antblessing@gmail.com',
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+  )
+  
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
   password: 'helloworld'
-)
+  )
 
 
 puts "Seed finished"
