d7fc45fcdb1962fc62170b8494dcfd4f32d23d3a
antblessing <antblessing@gmail.com>
2015-10-16 03:31:35 +0000

    API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006044313) do
+ActiveRecord::Schema.define(version: 20151016022305) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20151006044313) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
